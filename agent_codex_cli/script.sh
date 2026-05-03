c591d3c3d212b1337fb66f0df1a27b0ca80cf8dd
Zorbn <public.zorb@gmail.com>
2026-05-02 23:50:27 -0500

    Access NVM after install

--- agent_codex_cli/script.sh
+++ agent_codex_cli/script.sh
@@ -1,3 +1,2 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-nvm install --lts
+source <(curl -o- https://raw.githubusercontent.com/moreboxed/scripts/refs/heads/main/tool_node.js/script.sh)
 npm i -g @openai/codex
--- tool_node.js/script.sh
+++ tool_node.js/script.sh
@@ -1,3 +1,5 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install --lts
-npm install -g @google/gemini-cli
