3691a1253c0983757bb4749821601ebb4eeabd38
Zorbn <public.zorb@gmail.com>
2026-05-05 08:38:43 -0500

    Change sed args for Mac

--- tool_node.js/script.sh
+++ tool_node.js/script.sh
@@ -6,9 +6,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   rcfile="$HOME/.zshrc"
 fi
 
-sed -i '1i export NVM_DIR="$HOME/.nvm"' "$rcfile"
-sed -i '2i [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' "$rcfile"
-sed -i '3i [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' "$rcfile"
+sed -i '' '1i export NVM_DIR="$HOME/.nvm"' "$rcfile"
+sed -i '' '2i [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' "$rcfile"
+sed -i '' '3i [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' "$rcfile"
 
 . "$rcfile"
 nvm install --lts
