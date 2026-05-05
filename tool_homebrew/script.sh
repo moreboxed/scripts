fde0eba30d1faadaa47afadc0330ead6220c234b
Zorbn <public.zorb@gmail.com>
2026-05-04 23:04:58 -0500

    Supply Homebrew with sudo

--- tool_homebrew/script.sh
+++ tool_homebrew/script.sh
@@ -1,1 +1,1 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "$password" | sudo -S -v && NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
