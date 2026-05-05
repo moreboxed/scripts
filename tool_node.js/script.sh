5800d2ce3e681b8a466a1e77cfc6d1a6389b2692
Zorbn <public.zorb@gmail.com>
2026-05-04 21:49:32 -0500

    Add node to path

--- tool_node.js/script.sh
+++ tool_node.js/script.sh
@@ -1,5 +1,14 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+rcfile="$HOME/.bashrc"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  rcfile="$HOME/.zshrc"
+fi
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> "$rcfile"
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> "$rcfile"
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> "$rcfile"
+
+. "$rcfile"
 nvm install --lts
