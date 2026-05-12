6dd5cdf6ce47cd2d8f8ebe3b72c03ff1cc22bd64
Zorbn <public.zorb@gmail.com>
2026-05-12 12:52:21 -0500

    Add names to plist

--- tool_bun/script.sh
+++ tool_bun/script.sh
@@ -1,1 +1,13 @@
+echo "$password" | sudo -S -v
+
+if [ "$OSTYPE" != "Darwin" ]; then
+    if ! command -v unzip >/dev/null 2>&1; then
+        if command -v apt >/dev/null 2>&1; then
+            sudo apt update && sudo apt install -y unzip
+        elif command -v dnf >/dev/null 2>&1; then
+            sudo dnf install -y unzip
+        fi
+    fi
+fi
+
 curl -fsSL https://bun.sh/install | bash
